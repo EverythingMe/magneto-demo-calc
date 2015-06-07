0aa0890c940dd6755284abcaff47c56659d1f4cc
Matias Crivolotti <maticrivo@users.noreply.github.com>
2015-06-07 17:15:16 +0300

    make default calc_apk path to be absolute

--- conftest.py
+++ conftest.py
@@ -1,4 +1,9 @@
+import os
+
+calc_apk = os.path.abspath(os.path.join(os.path.dirname(__file__), 'calc.apk'))
+
 def pytest_configure(config):
     config.option.app_package = 'me.everything.magnetodemo'
     config.option.app_activity = 'me.everything.magnetodemo.Calculator'
-    config.option.apk_path = config.getoption('--apk-path') or 'calc.apk'
+    config.option.apk_path = config.getoption('--apk-path') or calc_apk
+    config.option.clean_install = True
