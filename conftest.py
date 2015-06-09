56bf25630c0f7b327ace4e2fe99609210508f544
Matias Crivolotti <maticrivo@users.noreply.github.com>
2015-06-09 18:08:54 +0300

    remove uninstall

--- conftest.py
+++ conftest.py
@@ -6,4 +6,3 @@ def pytest_configure(config):
     config.option.app_package = 'me.everything.magnetodemo'
     config.option.app_activity = 'me.everything.magnetodemo.Calculator'
     config.option.apk_path = config.getoption('--apk-path') or calc_apk
-    config.option.clean_install = True
